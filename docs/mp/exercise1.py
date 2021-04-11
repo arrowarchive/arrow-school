2e699330371753c10b714757ae74ff8042f7cace
arrowarchive <rgerzevske@gmail.com>
2021-04-11 12:40:46 -0400

    I'm confused with how this works, but it should work!

--- docs/mp/exercise1.py
+++ docs/mp/exercise1.py
@@ -1,4 +1,4 @@
-# start with Grimm project file. Pull its tagged words. Also pull its whole text.
+
 # objective: Learn to open and read in data from files.
 # play with spaCy
 # If necessary (probably not?) at Git Bash or terminal do: pip3 install spacy
@@ -8,8 +8,8 @@ import spacy
 nlp = spacy.cli.download("en_core_web_sm")
 nlp = spacy.load('en_core_web_sm')
 
-grimmRB = open('grimmRobberBridegroom.txt', 'r')
-words = grimmRB.read()
+tp5 = open('tp1.txt', 'r')
+words = tp5.read()
 wordstrings = str(words)
 print(wordstrings)
 
@@ -19,20 +19,20 @@ print(wordstrings)
 #     print(count, ": ", w)
 
 # start playing with spaCy and nlp:
-grimmWords = nlp(wordstrings)
-for token in grimmWords:
+tpsentences = nlp(wordstrings)
+for token in tpsentences:
      # if not token.is_punct:
     print(token.text, "---->", token.pos_, ":::::", token.lemma_)
 
 # On windows ctrl / comments out blocks.
 # On mac command / comments out blocks
-# grimmFile = open('grimm.txt', 'r')
-# doc2 = grimmFile.read()
+# grimmFile = open('tp1.txt', 'r')
+# doc2 = tp5.read()
 # docstring = str(doc2)
 # print(doc2)
 
-#nlpGrimm = nlp(docstring)
-# for token in nlpGrimm:
+#nlpTP = nlp(docstring)
+# for token in nlpTP:
     #print the token and its part of speech tag from spacy
     # print(token.text, "--->", token.pos_)
 
